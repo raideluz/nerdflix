50238e5189526862bf57045a4483d0fe023fd931
Raileny <example@example.com>
2021-09-25 18:01:43 -0300

    Initial Commit

---
+++ Nerdflix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nerdflix
+//
+//  Created by Natália Carolina Dos Santos on 25/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
